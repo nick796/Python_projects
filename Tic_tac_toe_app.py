120bcb12b5681c316e5ef406fb5e69f049163429
nick796 <32782139+nick796@users.noreply.github.com>
2023-06-17 15:28:42 +0300

    w

--- Tic_tac_toe_app.py
+++ Tic_tac_toe_app.py
@@ -3,6 +3,8 @@ from tkinter import ttk
 from tkinter import scrolledtext
 from tkinter import filedialog
 
+flag = False
+
 
 class TicTacToeApp(Tk):
     def __init__(self):
@@ -12,26 +14,93 @@ class TicTacToeApp(Tk):
         self.buttons = []
         self.create_buttons()
 
+        self.counter_row_O = 0
+        self.counter_column_O = 0
+        self.counter_diag_O = 0
+
+        self.counter_row_X = 0
+        self.counter_column_X = 0
+        self.counter_diag_X = 0
+
     def create_buttons(self):
         for i in range(3):
             for j in range(3):
                 index = i*3+j
                 button = ttk.Button(
-                    self, text=f" ", command=lambda idx=index: self.button_pressed(idx))
+                    self, text=" ", command=lambda idx=index, r=i, c=j: self.button_pressed(idx, r, c))
                 button.grid(
                     row=i, column=j, sticky="news")
+
                 self.buttons.append(button)
 
         for i in range(3):
             self.grid_columnconfigure(i, weight=1)
         for j in range(3):
             self.grid_rowconfigure(j, weight=1)
+        print(self.buttons)
 
-    def button_pressed(self, index):
-        if self.buttons[index]["text"] != "0":
+    def button_pressed(self, index, r, c):
+        global flag
+        if self.buttons[index]["text"] == " " and flag == False:
+            flag = True
             self.buttons[index]["text"] = "0"
-        else:
+            self.buttons[index]["state"] = DISABLED
+
+            counter_row_O = 0
+            for i in range(3):
+                if self.buttons[r*3+i]["text"] == "0":
+                    counter_row_O += 1
+                else:
+                    counter_row_O = 0
+
+            counter_column_O = 0
+            for j in range(3):
+                if self.buttons[j*3+c]["text"] == "0":
+                    counter_column_O += 1
+                else:
+                    counter_column_O = 0
+
+            counter_diag_O = 0
+            for i in range(3):
+                for j in range(3):
+                    if i == j:
+                        if self.buttons[i*3+j]["text"] == "0":
+                            counter_diag_O += 1
+                        else:
+                            counter_diag_O = 0
+
+            if counter_diag_O == 3 or counter_column_O == 3 or counter_row_O == 3:
+                print("0 is the winner")
+        elif self.buttons[index]["text"] == " " and flag == True:
+            flag = False
             self.buttons[index]["text"] = "X"
+            self.buttons[index]["state"] = DISABLED
+
+            counter_row_O = 0
+            for i in range(3):
+                if self.buttons[r*3+i]["text"] == "X":
+                    counter_row_O += 1
+                else:
+                    counter_row_O = 0
+
+            counter_column_O = 0
+            for j in range(3):
+                if self.buttons[j*3+c]["text"] == "X":
+                    counter_column_O += 1
+                else:
+                    counter_column_O = 0
+
+            counter_diag_O = 0
+            for i in range(3):
+                for j in range(3):
+                    if i == j:
+                        if self.buttons[i*3+j]["text"] == "X":
+                            counter_diag_O += 1
+                        else:
+                            counter_diag_O = 0
+
+            if counter_diag_O == 3 or counter_column_O == 3 or counter_row_O == 3:
+                print("X is the winner")
 
     # def button_pressed(self, index):
     #     def callback():
--- test.py
+++ test.py
@@ -1,27 +1,15 @@
-from tkinter import *
+import tkinter as tk
 
+root = tk.Tk()
 
-class MyApp:
-    def __init__(self, master):
-        self.master = master
-        self.buttons = []
-        self.create_buttons()
-
-    def create_buttons(self):
-        for i in range(3):
-            for j in range(3):
-                idx = i * 3 + j
-                button = Button(self.master, text=f"Button {idx+1}")
-                button.grid(row=i, column=j)
-                button.config(command=self.button_pressed(idx))
-                self.buttons.append(button)
-
-    def button_pressed(self, idx):
-        def callback():
-            self.buttons[idx].config(text=f"Button {idx+1} Pressed")
-        return callback
-
-
-root = Tk()
-app = MyApp(root)
+# Create a 3x3 matrix of buttons
+matrix = []
+for i in range(3):
+    row = []
+    for j in range(3):
+        button = tk.Button(root, text=f"Button {i+1},{j+1}")
+        button.grid(row=i, column=j, padx=10, pady=10)
+        row.append(button)
+    matrix.append(row)
+print(matrix)
 root.mainloop()
