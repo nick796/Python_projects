a2cf59f684867405b01ddac42e8b0f4e5c048ec0
nick796 <32782139+nick796@users.noreply.github.com>
2023-06-18 13:58:10 +0300

    finish

--- Password_Generator.py
+++ Password_Generator.py
@@ -1,5 +1,7 @@
 from tkinter import *
 from tkinter import ttk
+import random
+import string
 
 
 class PlaceholderSpinbox(ttk.Spinbox):
@@ -41,6 +43,8 @@ class PasswordGeneratorApp(Tk):
         self.create_spinboxes()
         self.create_button()
 
+        print(string.ascii_lowercase)
+
         self.grid_rowconfigure(0, weight=1)
         self.grid_rowconfigure(1, weight=1)
         self.grid_rowconfigure(2, weight=1)
@@ -74,29 +78,92 @@ class PasswordGeneratorApp(Tk):
 
     def create_spinboxes(self):
 
-        num_char_spinbox = PlaceholderSpinbox(
+        self.num_char_spinbox = PlaceholderSpinbox(
             self, from_=1, to=12, style="Placeholder.TSpinbox", placeholder="Select a value")
-        num_char_spinbox.grid(row=1, column=0, sticky="n")
+        self.num_char_spinbox.grid(row=1, column=0, sticky="n")
 
-        number_uppercase_char_spinbox = PlaceholderSpinbox(
+        self.number_uppercase_char_spinbox = PlaceholderSpinbox(
             self, from_=0, to=12, style="Placeholder.TSpinbox", placeholder="Select a value")
-        number_uppercase_char_spinbox.grid(row=1, column=1, sticky="n")
+        self.number_uppercase_char_spinbox.grid(row=1, column=1, sticky="n")
 
-        number_symbols_char_spinbox = PlaceholderSpinbox(
+        self.number_symbols_char_spinbox = PlaceholderSpinbox(
             self, from_=0, to=12, style="Placeholder.TSpinbox", placeholder="Select a value")
-        number_symbols_char_spinbox.grid(row=1, column=2, sticky="n")
+        self.number_symbols_char_spinbox.grid(row=1, column=2, sticky="n")
 
-        number_numbers_spinbox = PlaceholderSpinbox(
+        self.number_numbers_spinbox = PlaceholderSpinbox(
             self, from_=0, to=12, style="Placeholder.TSpinbox", placeholder="Select a value")
-        number_numbers_spinbox.grid(row=1, column=3, sticky="n")
+        self.number_numbers_spinbox.grid(row=1, column=3, sticky="n")
 
     def create_button(self):
-        button = ttk.Button(self, text="Create",
-                            command=self.create_pass, padding=10, style="Button.TButton")
-        button.grid(row=2, columnspan=4, sticky="n")
+        self.button = ttk.Button(self, text="Create",
+                                 command=self.create_pass, padding=10, style="Button.TButton")
+        self.button.grid(row=2, columnspan=4, sticky="n")
 
     def create_pass(self):
-        pass
+        num_char_value = self.num_char_spinbox.get()
+        num_uppercase_value = self.number_uppercase_char_spinbox.get()
+        num_symbols_value = self.number_symbols_char_spinbox.get()
+        num_numbers_value = self.number_numbers_spinbox.get()
+
+        password_length = int(num_char_value)
+        uppercase_chars = int(num_uppercase_value)
+        symbol_chars = int(num_symbols_value)
+        number_chars = int(num_numbers_value)
+
+        lowercase_chars = password_length - uppercase_chars - symbol_chars - number_chars
+        lowercase_letters = string.ascii_lowercase
+        password = ''.join(random.choice(lowercase_letters)
+                           for _ in range(lowercase_chars))
+
+        # Generate uppercase characters
+        uppercase_letters = string.ascii_uppercase
+        password += ''.join(random.choice(uppercase_letters)
+                            for _ in range(uppercase_chars))
+
+        # Generate symbol characters
+        symbols = "!@#%"
+        password += ''.join(random.choice(symbols)
+                            for _ in range(symbol_chars))
+
+        # Generate number characters
+        numbers = string.digits
+        password += ''.join(random.choice(numbers)
+                            for _ in range(number_chars))
+
+        # Shuffle the password to randomize the character order
+        password_list = list(password)
+        random.shuffle(password_list)
+        password = ''.join(password_list)
+
+        new_window = Toplevel(self)
+        new_window.title("The password ")
+        new_window.grab_set()
+        prev_x = self.winfo_x()
+        prev_y = self.winfo_y()
+        prev_width = self.winfo_width()
+        prev_height = self.winfo_height()
+
+        new_x = prev_x + (prev_width - new_window.winfo_reqwidth()) // 2
+        new_y = prev_y + (prev_height - new_window.winfo_reqheight()) // 2
+
+        # Place the new window at the calculated position
+        new_window.geometry(f"+{new_x}+{new_y}")
+
+        label = ttk.Label(new_window, text=f"Your password is {password}")
+        label.pack()
+
+        restart_button = ttk.Button(
+            new_window, text="Restart", command=self.restart)
+        quit_button = ttk.Button(new_window, text="Quit", command=self.quit)
+        restart_button.pack()
+        quit_button.pack()
+
+        print("Generated Password:", password)
+
+    def restart(self):
+        self.destroy()
+        new_window = PasswordGeneratorApp()
+        new_window.mainloop()
 
 
 if __name__ == '__main__':
--- test.py
+++ test.py
@@ -1,18 +1,13 @@
 from tkinter import *
 from tkinter import ttk
+import random
+import string
 
+lowercase_chars = 5
+lowercase_letters = "abcdefg"
+password = ''.join(random.choice(lowercase_letters)
+                   for _ in range(lowercase_chars))
 
-class MyApp(Tk):
-    def __init__(self):
-        super().__init__()
-        self.title("Adjustable Height Spinbox")
-        self.geometry("400x200")
-
-        spinbox = ttk.Spinbox(self, from_=1, to=12, font=("Arial", 12))
-        spinbox.grid(row=0, column=0, padx=10, pady=(20, 10))
-        spinbox.config(padding=(10, 5))  # Adjust the top and bottom padding
-
-
-if __name__ == '__main__':
-    app = MyApp()
-    app.mainloop()
+print(password)
+password_list = list(password)
+print(password_list)
